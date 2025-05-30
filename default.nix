09488a426e64a63cb322a9a3604fc5a885432c19
SomeoneSerge <else@someonex.net>
2025-05-30 15:54:54 +0000

    cudaPackages: showcase custom manifests and bumping to 12.9
    Only tested nix-instantiate -A python3Packages.torch...
    
    JSONs obtained by running, in this repo's checkout,
    
    `nom build -f . cudaPackages._rawManifests.snapshots.2025-05-23.export`

--- default.nix
+++ default.nix
@@ -1,19 +1,26 @@
 let
   npins = import ./npins;
-  _cuda = import (npins."nixpkgs" + "/pkgs/development/cuda-modules/_cuda");
+  internalsOld = import (npins."nixpkgs" + "/pkgs/development/cuda-modules/_cuda");
+  mkCudb = import (npins."nixpkgs" + "/pkgs/development/cuda-modules/_cuda/db");
   lib = import (npins."nixos-24.11" + "/lib");
   pkgs = import npins."nixos-24.11" {
     overlays = builtins.attrValues overlays;
-    config.allowUnfreePredicate = _cuda.lib.allowUnfreeCudaPredicate;
+    config.allowUnfreePredicate = internalsOld.lib.allowUnfreeCudaPredicate;
     config.cudaSupport = true;
   };
   overlays.eradicateOldCuda =
     _final: prev:
     lib.mapAttrs (_: _: null) (lib.filterAttrs (name: _: lib.hasPrefix "cudaPackages" name) prev);
   overlays.newCuda = final: _prev: {
-    inherit _cuda;
+    _cuda = internalsOld.extend (final: prev: {
+      dbEvaluation = mkCudb { manifests = [ ]; extraModules = map lib.importJSON [
+        ./cudb/blobs.json
+        ./cudb/outputs.json
+        ./cudb/packages.json
+      ]; };
+    });
     cudaPackages = final.callPackage (npins."nixpkgs" + "/pkgs/top-level/cuda-packages.nix") {
-      pinProducts.cuda = "12.8";
+      pinProducts.cuda = "12.9";
     };
   };
 in
